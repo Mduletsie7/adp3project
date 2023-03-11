62a40a04ecfbfd2416fce65a4167814dac3abf99
Mduletsie7 <mdu.letsie7@gmail.com>
2023-03-11 18:27:45 +0200

    Reverted Animal Class to original blank state

--- src/main/java/za/ac/cput/domain/Animal.java
+++ src/main/java/za/ac/cput/domain/Animal.java
@@ -1,42 +1,4 @@
 package za.ac.cput.domain;
 
 public class Animal {
-    private String colour;
-    private String species;
-    private String sound;
-    private int age;
-    private int noOfLegs;
-
-    public Animal() {}
-
-    public String getColour() {
-        return colour;
-    }
-
-    public String getSpecies() {
-        return species;
-    }
-
-    public String getSound() {
-        return sound;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public int getNoOfLegs() {
-        return noOfLegs;
-    }
-
-    @Override
-    public String toString() {
-        return "Animal{" +
-                "colour='" + colour + '\'' +
-                ", species='" + species + '\'' +
-                ", sound='" + sound + '\'' +
-                ", age=" + age +
-                ", noOfLegs=" + noOfLegs +
-                '}';
-    }
 }
