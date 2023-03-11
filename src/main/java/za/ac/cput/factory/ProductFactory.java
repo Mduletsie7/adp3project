884f974712e47d57bc4f98791f5c11aabc31b827
Mduletsie7 <mdu.letsie7@gmail.com>
2023-03-11 21:13:34 +0200

    Added Product with Unit Tests

--- src/main/java/za/ac/cput/factory/ProductFactory.java
+++ src/main/java/za/ac/cput/factory/ProductFactory.java
@@ -4,14 +4,13 @@ import za.ac.cput.domain.Product;
 import za.ac.cput.util.Helper;
 
 public class ProductFactory {
-    public static Product createProduct(String productName, String productCategory, String productBrand) {
+    public static Product createProduct(String productName, Double productPrice, String productCategory, String productBrand) {
         if (Helper.isNullOrEmpty(productName) || Helper.isNullOrEmpty(productCategory) || Helper.isNullOrEmpty(productBrand)) {
             return null;
         }
 
         String productId = Helper.generateId();
         boolean inStock = Helper.checkStock();
-        Double productPrice = 0.0;
 
         Product product = new Product.Builder()
                 .setProductID(productId)
