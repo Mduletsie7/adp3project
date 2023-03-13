a183023e3905dd06df2758a2afd6f1ca02962df1
Lerato - 220076073 <35888479+Lerato007@users.noreply.github.com>
2023-03-13 17:24:34 +0200

    Additional tests were added to the code

--- src/test/java/za/ac/cput/factory/AnimalFactoryTest.java
+++ src/test/java/za/ac/cput/factory/AnimalFactoryTest.java
@@ -7,8 +7,11 @@
 
 package za.ac.cput.factory;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import za.ac.cput.domain.Animal;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,5 +29,55 @@ class AnimalFactoryTest {
         Animal animal = AnimalFactory.createAnimal("", "Dog","Woof");
         System.out.println(animal.toString());
         assertNotNull(animal);
+    }
+
+    // Object Equality Test
+    @Test
+    public void testCompareAnimalSpecies() {
+        Animal animalSpecies = new Animal();
+        Animal mySpecies = AnimalFactory.createAnimal("Black", "Cat", "Mhew");
+        String speciesName = mySpecies.getSpecies().toString();
+        assertEquals(speciesName, "Janguar");
+    }
+
+
+    // DISABLING TEST
+    @Disabled("TODO: Still need to complete this method code")
+    @Test
+    public void test_animalColour() {
+    }
+
+    // TIMEOUT TEST
+    @Test
+    void testTimeOut() {
+        assertTimeout(Duration.ofMillis(100), () -> {
+            Thread.sleep(2000);
+
+            System.out.println("Animals rule!");
+        });
+    }
+
+
+    // Object Identity
+    @Test
+    public void testObjectIdentity() {
+        Animal animal = new Animal();
+        Animal animal1 = AnimalFactory.createAnimal("Blue", "Whale", "dfghtrg");
+        Animal animal2 =  animal1;
+        Animal animal3 = AnimalFactory.createAnimal("White", "Shark", "rete");
+
+        if(animal1 == animal1) {
+            System.out.println("Animals are identical");
+        }
+        if(animal1 == animal2) {
+            System.out.println("Animals are identical");
+        } else {
+            System.out.println("Animals are not identical");
+        }
+        if(animal1 == animal3) {
+            System.out.println("Animals are identical");
+        } else {
+            System.out.println("Animals are not identical");
+        }
     }
 }
